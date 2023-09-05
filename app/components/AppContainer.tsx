877d5bf69be14f9edaab21133300b568150f51f7
glowlabs-stephen <>
2023-09-05 12:00:08 +0530

    deploy erreor test

--- app/components/AppContainer.tsx
+++ app/components/AppContainer.tsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import JobForm from './JobForm';
-import JobCard from './JobCard';
-import { Job } from './utils/types/FormTypes';
-import { CardButton } from './UI/Button';
-import { initalState } from './UI/Constants';
+import React, { useState } from "react";
+import JobForm from "./JobForm";
+import JobCard from "./JobCard";
+import { Job } from "./utils/types/FormTypes";
+import { CardButton } from "./UI/Button";
+import { initalState } from "./UI/Constants";
 
 function AppContainer({ jobsList }: Job[] | any) {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,16 +21,16 @@ function AppContainer({ jobsList }: Job[] | any) {
 
   const styles = {
     gridContainer:
-      'grid lg:grid-cols-2 sm:grid-cols-1 pl-[85px] pr-[44.882px] pt-[30px] pb-[49px] gap-7',
+      "grid lg:grid-cols-2 sm:grid-cols-1 pl-[85px] pr-[44.882px] pt-[30px] pb-[49px] gap-7",
   };
 
   return (
     <div className="relative min-h-screen">
-      <CardButton
+      {/* <CardButton
         onClick={handleClick}
         btnText="Create Job"
         variant="primary"
-      />
+      /> */}
 
       {/* {isOpen && (
         <JobForm
@@ -53,6 +53,7 @@ function AppContainer({ jobsList }: Job[] | any) {
           </div>
         ))}
       </div> */}
+      {"testContainer"}
     </div>
   );
 }
